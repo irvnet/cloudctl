c4cfa86b29414a9510b4b6c810644834f87fa508
irvnet <irvnet@gmail.com>
2025-04-15 09:28:26 -0400

    fix: passing ssh key

--- pkg/hetzner/vm.go
+++ pkg/hetzner/vm.go
@@ -2,24 +2,28 @@ package hetzner
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hetznercloud/hcloud-go/hcloud"
 )
 
 func CreateVM(ctx context.Context, client *hcloud.Client, name string) (*hcloud.Server, error) {
-	serverType := "cx21"              // Small dev server
-	image := "ubuntu-22.04"           // Good default
-	location := "nbg1"                // Germany DC
-	sshKeyName := "your-ssh-key-name" // SSH key pre-loaded in Hetzner panel
+	serverType := "cax11"       // Small dev server
+	image := "ubuntu-22.04"     // Good default
+	location := "nbg1"          // Germany DC
+	sshKeyName := "hetzner-ssh" // SSH key pre-loaded in Hetzner panel
+
+	sshKey, _, err := client.SSHKey.GetByName(ctx, sshKeyName)
+	if err != nil || sshKey == nil {
+		return nil, fmt.Errorf("SSH key '%s' not found in your account...", sshKeyName)
+	}
 
 	opts := hcloud.ServerCreateOpts{
 		Name:       name,
 		ServerType: &hcloud.ServerType{Name: serverType},
 		Image:      &hcloud.Image{Name: image},
 		Location:   &hcloud.Location{Name: location},
-		SSHKeys: []*hcloud.SSHKey{
-			{Name: sshKeyName},
-		},
+		SSHKeys:    []*hcloud.SSHKey{sshKey},
 	}
 
 	result, _, err := client.Server.Create(ctx, opts)
