c4e745c98b93ce90473434693b5efb068ab9736a
irvnet <irvnet@gmail.com>
2025-04-21 14:45:26 -0400

    update: search for a vm before creating

--- cmd/provision_vm.go
+++ cmd/provision_vm.go
@@ -17,13 +17,22 @@ var provisionVmCmd = &cobra.Command{
 		client := hetzner.NewClient()
 		ctx := context.Background()
 
-		server, err := hetzner.CreateVM(ctx, client, vmName)
+		server, _, err := client.Server.GetByName(ctx, vmName)
 		if err != nil {
-			fmt.Printf("❌ Failed to create VM: %v\n", err)
-			return
+			fmt.Printf("Err: finding %s by name: %s", vmName, err)
 		}
 
-		fmt.Printf("✅ Created VM: %s (ID: %d)\n", server.Name, server.ID)
+		if server != nil {
+			fmt.Printf("ID:%v: / %v / %v already created", server.ID, server.Name, server.Status)
+		} else {
+			server, err := hetzner.CreateVM(ctx, client, vmName)
+			if err != nil {
+				fmt.Printf("❌ Failed to create VM: %v\n", err)
+				return
+			}
+			fmt.Printf("✅ Created VM: %s (ID: %d)\n", server.Name, server.ID)
+
+		}
 	},
 }
 
