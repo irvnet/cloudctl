73183564354fa8ff587c3453bd192a189a9f94fe
irvnet <irvnet@gmail.com>
2025-04-23 16:28:03 -0400

    fix: update to valid vm type

--- internal/infra/defaults.go
+++ internal/infra/defaults.go
@@ -10,15 +10,15 @@ type ComponentConfig struct {
 
 var ComponentDefaults = map[string]ComponentConfig{
 	"devbox": {
-		ServerType: "cx11", Image: "ubuntu-22.04", Location: "fsn1", SSHKey: "virtuser-bluefin",
+		ServerType: "cax11", Image: "ubuntu-22.04", Location: "fsn1", SSHKey: "virtuser-bluefin",
 		Labels: map[string]string{"role": "devbox"},
 	},
 	"ctrl": {
-		ServerType: "cx21", Image: "ubuntu-22.04", Location: "fsn1", SSHKey: "virtuser-bluefin",
+		ServerType: "cax11", Image: "ubuntu-22.04", Location: "fsn1", SSHKey: "virtuser-bluefin",
 		Labels: map[string]string{"role": "ctrl"},
 	},
 	"worker": {
-		ServerType: "cx21", Image: "ubuntu-22.04", Location: "fsn1", SSHKey: "virtuser-bluefin",
+		ServerType: "cax11", Image: "ubuntu-22.04", Location: "fsn1", SSHKey: "virtuser-bluefin",
 		Labels: map[string]string{"role": "worker"},
 	},
 }
